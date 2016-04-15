e793c330c31235a5bdbe447ce9426625eb68ce97
mallory13 <mallory.steele13@gmail.com>
2016-04-15 12:33:24 -0400

    js picture supported

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // property or element JavaScript detection with optional Polyfill
+
+if (Modernizr.picture) {
+  // supported
+  console.log("Picture element supported")
+} else {
+  // not-supported
+  console.log("Picture element not supported")
+}
